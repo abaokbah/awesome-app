5e7d65dce8e0c681e6afaa5e18b65b101f1999b4
Ali Baokbah <art-is-ablast@hotmail.com>
2020-11-06 20:18:02 +0300

    Pre services version of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Post } from "./posts/post.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'awesome-app';
+  /**
+   * onPostAdded
+   */
+  storedPosts: Post[] = [];
+  public onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
+
+
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,27 @@
-import { Component, NgModule } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
+import { NgForm } from "@angular/forms"
+
+import { Post } from "../post.model";
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
 
 export class PostCreateComponent{
-  newPost = 'Tell us something..';
-  enteredText = '';
+  enteredTitle = '';
+  enteredContent = '';
 
-  public onClickAdd() {
-    this.newPost = this.enteredText;
+  @Output() postCreated = new EventEmitter();
+  public onClickAdd(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    }
+    this.postCreated.emit(post);
   }
 }
